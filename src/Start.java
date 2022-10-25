a468c8e58e8c95b475f6a1a64fb9ba7a69fe0e94
chanwho <anxi0015@gmail.com>
2022-10-25 13:45:37 +0900

    ch

--- src/Start.java
+++ src/Start.java
@@ -1,3 +1,11 @@
+import java.util.Scanner;
+
 public class Start {
-    
+    public void startScreen(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("---------------------------------------------------------");
+        System.out.println("P.설문시작 | S.통계 | Q.종료");
+        String StartCommend = sc.next();
+        System.out.println("---------------------------------------------------------");
+    }
 }
