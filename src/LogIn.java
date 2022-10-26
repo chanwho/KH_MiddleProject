ca799ceed40c86ee7012369beaed0e1f32d2086c
HNNNY35 <hnnny35@gmail.com>
2022-10-26 13:50:51 +0900

    haein

--- src/LogIn.java
+++ src/LogIn.java
@@ -4,9 +4,44 @@ import java.sql.*;
 
 public class LogIn {
 // 로그인화면구성
-    public void LoginFunction(Statement statement) {
+    public String LoginFunction(Statement statement) {
+        Scanner sc = new Scanner(System.in);
+        String ID = "";
+        String PW = "";
+        String query = "";
+        ResultSet resultSet;
 
+        System.out.println("------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("로그인 화면입니다.\n아이디와 비밀번호를 입력해주세요.");
+        System.out.println("------------------------------------------------------------------------------------------------------------------------");
+        
+        // 아이디와 비밀번호 받기
+        try {
+            do {
+                ID = sc.nextLine();
+                PW = sc.nextLine();
 
+        // 아이디와 비밀번호 비교
+                query = "SELECT * FROM user WHERE ID = '" + ID + "'";
+                resultSet = statement.executeQuery(query);
+
+                if(resultSet != null) {
+                    System.out.println("일치하는 아이디가 없습니다. \n아이디와 비밀번호를 입력해주세요.");
+                    } else {
+                        query = "SELECT * FROM user WHERE ID = '" + ID + "' AND PW = '" + PW + "'";
+                        resultSet = statement.executeQuery(query);
+                        if(resultSet == null) {
+                            System.out.println("- 잘못된 비밀번호입니다. \n아이디와 비밀번호를 입력해주세요.");
+                        }
+                    }
+            } while(resultSet != null);
+            System.out.println("로그인");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        String useruid = "";
+        return useruid;
 }
 
 
--- src/Main.java
+++ src/Main.java
@@ -12,20 +12,21 @@ public class Main{
         try {
             Connection connection = DriverManager.getConnection(url, user, password);
             Statement statement = connection.createStatement();
-            while(true){
+            //while(true){
                 
                 // 로그인화면
-                
+                LogIn login = new LogIn();
+                login.LoginFunction(statement);
                 // Start start = new Start();
                 // start.startFunction();
-                Survey survey = new Survey();
-                survey.SurveyFunction(statement);
-                break;
+                // Survey survey = new Survey();
+                // survey.SurveyFunction(statement);
+                // break;
                 // Statics statics = new Statics();
                 // statics.StaticsFunction(statement);
     
                 // 종료화면
-            }
+            //}
             } catch (SQLException e) {
             e.printStackTrace();
         }
