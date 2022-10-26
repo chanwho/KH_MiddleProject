cae48a959ef82c69860e9e45fa674d96a3b0683d
HNNNY35 <hnnny35@gmail.com>
2022-10-26 11:51:41 +0900

    haein

---
+++ haein/221026/LogIn.java
@@ -0,0 +1,18 @@
+package 221026;
+
+
+
+public class LogIn {
+//     - 로그인화면구성
+public static void main(String[] args) {
+    public void LoginFunction() {
+        System.out.println("로그인 화면입니다. \n 아이디와 비밀번호를 입력해주세요");
+    }
+}
+
+// 아이디와 비밀번호 받기
+// 아이디와 비밀번호 비교
+// 일치하면 유저 이름 및 권한 출력
+// 불일치시 재 로그인 요청(반복)
+
+}
---
+++ src/LogIn.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.sql.*;
+
+
+public class LogIn {
+// 로그인화면구성
+    public void LoginFunction(Statement statement) {
+
+
+}
+
+
+
+// 일치하면 유저 이름 및 권한 출력
+// 불일치시 재 로그인 요청(반복)
+
+}
