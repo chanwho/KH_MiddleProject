15bf93a8d731176c01f1734030d620d800d81139
HNNNY35 <hnnny35@gmail.com>
2022-10-27 09:34:29 +0900

    hi

--- src/Login.java
+++ src/Login.java
@@ -22,21 +22,24 @@ public class LogIn {
                 try {
                     do {
                 // 아이디와 비밀번호 받기
+                System.out.print("ID) ");
                 ID = sc.nextLine();
+                System.out.print("PW) ");
                 PW = sc.nextLine();
+                System.out.println();
 
                 // 아이디 비교
                 query = "SELECT * FROM user WHERE ID = '" + ID + "';";
                 resultSet = statement.executeQuery(query);
 
                 if (!resultSet.isBeforeFirst()) {
-                    System.out.println("일치하는 아이디가 없습니다. \n아이디와 비밀번호를 입력해주세요.");
+                    System.out.println("일치하는 아이디가 없습니다. \n아이디와 비밀번호를 입력해주세요.\n");
                 } else {
                     // 아이디가 있는 경우, 아이디와 비밀번호가 일치하는지 확인
                     query = "SELECT * FROM user WHERE ID = '" + ID + "' AND PW = '" + PW + "'";
                     resultSet = statement.executeQuery(query);
                     if (!resultSet.isBeforeFirst()) {
-                        System.out.println("잘못된 비밀번호입니다. \n아이디와 비밀번호를 입력해주세요.");
+                        System.out.println("잘못된 비밀번호입니다. \n아이디와 비밀번호를 입력해주세요.\n");
                     } 
                 }
             } while (!resultSet.isBeforeFirst());
@@ -54,12 +57,11 @@ public class LogIn {
                 user_author = resultSet.getString("AUTHOR");
             }
             
-            System.out.println(user_id + "님 " + user_author + " 권한으로 로그인합니다.");
+            System.out.println(user_id + "님 [" + user_author + "] 권한으로 로그인합니다.\n");
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        sc.close();
         return user_uid;
     }
 
